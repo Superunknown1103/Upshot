61a4df09d71f0641e2f51c4a8b4fabc06f86038c
Bruce <superunknown1103@gmail.com>
2020-01-05 18:24:22 -0600

    Updates

---
+++ precache-manifest.ef89b5011231302db6d84a5426c1f357.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3504ece05bd17765f6e2382466dc07",
+    "url": "/Upshot/index.html"
+  },
+  {
+    "revision": "e81dfbf5b4ba96c099bb",
+    "url": "/Upshot/static/css/main.cedd5d61.chunk.css"
+  },
+  {
+    "revision": "a7fab5f04c62b61d131c",
+    "url": "/Upshot/static/js/2.57b36ef7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Upshot/static/js/2.57b36ef7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e81dfbf5b4ba96c099bb",
+    "url": "/Upshot/static/js/main.d6aacfc8.chunk.js"
+  },
+  {
+    "revision": "55a12bf4f20e1d57bc50",
+    "url": "/Upshot/static/js/runtime-main.3a347e1b.js"
+  },
+  {
+    "revision": "3550be5b843e15ec74b2866c7982bbd1",
+    "url": "/Upshot/static/media/leaf-banner.3550be5b.jpg"
+  },
+  {
+    "revision": "f54e80eb8872f14d51c9a37ab901521c",
+    "url": "/Upshot/static/media/shoe.f54e80eb.jpg"
+  }
+]);
